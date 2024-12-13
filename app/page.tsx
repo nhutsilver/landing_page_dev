e9b296f969ee8f3689f89da47c1626b5a81c6654
nhutsilver <hmnhut.dev@gmail.com>
2024-12-13 21:29:53 +0700

    second push

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Hero from "./components/hero/Hero";
 import Services from "./components/services/Services";
 import AboutUs from "./components/about_us/AboutUs";
-import Customers from "./components/customers/Customers";
+// import Customers from "./components/customers/Customers";
 import ContactForm from "./components/contact_form/ContactForm";
 import "./globals.css";
 export default function Home() {
